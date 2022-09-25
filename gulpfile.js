9dbabb0c0e77d75d713bc31d4abd6f4533924cb2
James Alderson <james.alderson.a@gmail.com>
2022-09-25 18:10:49 +0330

    Define 'Gulp' task runner
    - Create 'gulpfile.js'.
    - Create '.gitignore'.
    - Create 'dist' directory.
    - Define 'package.json' and 'package-lock.json'.

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+// Initialize modules
+const { src, dest, watch, series, parallel } = require("gulp")
+
+// Import packages
+const postcss = require("gulp-postcss")
+const autoprefixer = require("autoprefixer")
+const cssnano = require("cssnano")
+const replace = require("gulp-replace")
+const browser_sync = require("browser-sync").create()
+
+// Files path
+const files = {
+  cssPath: "src/css/*.css",
+}
+
+// Css task
+function css_task() {
+  return src(files.cssPath)
+    .pipe(postcss([autoprefixer("last 4 versions"), cssnano()]))
+    .pipe(dest("dist/css"))
+}
+
+// Cachebust task
+function cachebust_task() {
+  let dateString = new Date().getTime()
+
+  return src(["index.html"])
+    .pipe(replace(/cb=\d+/g, "cb=" + dateString))
+    .pipe(dest("."))
+}
+
+// CopyCss task
+function copyCss_task() {
+  return src("src/css/vendors/**/*")
+    .pipe(dest("dist/css/vendors"))
+}
+
+// CopyImage task
+function copyImage_task() {
+  return src("src/images/favicon/*")
+    .pipe(dest("dist/images/favicon"))
+}
+
+// Initialize browserSync
+function browserSync(callback) {
+  browser_sync.init({
+    server: {
+      baseDir: "."
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: "0"
+      }
+    }
+  })
+  callback()
+}
+
+// Reload browserSync
+function browserSyncReload(callback) {
+  browser_sync.reload()
+  callback()
+}
+
+// BrowserSync watch
+function browserSyncWatch() {
+  watch(["index.html"], browserSyncReload)
+  watch([files.cssPath], series(parallel(css_task), cachebust_task))
+}
+
+// Gulp watch task
+function watch_task() {
+  watch([files.cssPath], series(parallel(css_task), cachebust_task))
+}
+
+// Gulp default task
+exports.default = series(parallel(css_task), cachebust_task, copyCss_task, copyImage_task, watch_task)
+
+// Run browserSync
+exports.bs = series(parallel(css_task), cachebust_task, copyCss_task, copyImage_task, browserSync, browserSyncWatch)
