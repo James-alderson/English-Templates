2eb2a27e540f725de1c201e8d4a48f832cecae1a
James-alderson <james.alderson.a@gmail.com>
2022-11-11 17:59:15 +0330

    Install development packages
    - Install packages:
      -- 'tailwindcss'.
      -- 'prettier'.
      -- 'prettier-plugin-tailwindcss'.

---
+++ Templates/02-Photosen/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    colors: {
+      "black": "#000000"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
