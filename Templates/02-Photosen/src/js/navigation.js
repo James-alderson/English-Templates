56563fe247397fee51503f142a3aa57df29e9866
James-alderson <james.alderson.a@gmail.com>
2022-11-29 16:12:44 +0330

    Improve template performance
    - Update animation styles.
    - Update 'site.webmanifest'.
    - Update 'slideNavigation' method.
    - Clean 'navigation.js' code.
    - Change 'font-display' value to 'swap'.
    - Fix 'Accessibility' and 'Best practice' issues.
    - Add 'loading' attribute for img elements.
    - Add 'width' and 'height' attribute for img elements
    - Add 'aria-label' for menu buttons and social media icons.

--- Templates/02-Photosen/src/js/navigation.js
+++ Templates/02-Photosen/src/js/navigation.js
@@ -1,39 +1,84 @@
+/* ------ Table of content ------
+
+1. Variables
+2. Events
+3. Loops
+4. Global functions
+5. Local functions
+
+------------------------------ */
+
+/* ===============
+   Variables
+=============== */
+const DESKTOP_RESOLUTION = 1280
 const HEADER = document.querySelector('header')
 const SHADOW = document.querySelector('#shadow')
 const OPEN_MENU = document.querySelector('#openMenu')
 const CLOSE_MENU = document.querySelector('#closeMenu')
 const DROPDOWNS = document.querySelectorAll('.dropdown')
+const HEADER_CONTAINER = document.querySelector('.header-container')
 const DROPDOWN_BUTTONS = document.querySelectorAll('.dropdown-btn')
 const DROPDOWN_CONTENTS = document.querySelectorAll('.dropdown-content')
 
+/* ===============
+   Events
+=============== */
 SHADOW.addEventListener('click', toggle_slideNavigation)
 OPEN_MENU.addEventListener('click', toggle_slideNavigation)
 CLOSE_MENU.addEventListener('click', toggle_slideNavigation)
 window.addEventListener('click', close_dropdown)
+window.addEventListener('load', header_animation)
 window.addEventListener('resize', () => {
-  transition_dropdown()
   toggle_content()
+  transition_dropdown()
+  transition_slideNavigation()
 })
 
+/* ===============
+   Loops
+=============== */
 DROPDOWN_BUTTONS.forEach((dropdown) => {
   dropdown.addEventListener('click', function () {
     toggle_dropdown(dropdown)
   })
 })
 
+/* ====================
+   Global functions
+==================== */
+const remove_class = (element, className, delay = 400) => {
+  setTimeout(() => element.classList.remove(className), delay)
+}
+
+/* ====================
+   Local functions
+==================== */
+function header_animation() {
+  if (window.innerWidth >= DESKTOP_RESOLUTION)
+    HEADER.classList.add('header-fadein')
+  else HEADER_CONTAINER.classList.add('header-fadein')
+
+  remove_class(HEADER, 'header-fadein', 1300)
+  remove_class(HEADER_CONTAINER, 'header-fadein', 1300)
+}
+
+function toggle_content() {
+  if (
+    window.innerWidth < DESKTOP_RESOLUTION &&
+    HEADER.classList.contains('open')
+  )
+    document.body.classList.add('hide-content')
+  if (window.innerWidth >= DESKTOP_RESOLUTION)
+    remove_class(document.body, 'hide-content')
+}
+
 function toggle_slideNavigation() {
   HEADER.classList.toggle('open')
 
   if (HEADER.classList.contains('open'))
     document.body.classList.add('hide-content')
-  else setTimeout(() => document.body.classList.remove('hide-content'), 400)
-}
-
-function toggle_content() {
-  if (window.innerWidth < 1280 && HEADER.classList.contains('open'))
-    document.body.classList.add('hide-content')
-  if (window.innerWidth >= 1280)
-    setTimeout(() => document.body.classList.remove('hide-content'), 400)
+  else remove_class(document.body, 'hide-content')
 }
 
 function toggle_dropdown(element) {
@@ -57,8 +102,12 @@ function toggle_dropdown(element) {
   }
 }
 
+// Close dropdown when click outer section of dropdown (in mobile resolution)
 function close_dropdown(event) {
-  if (!event.target.closest('#slideNavigation') && window.innerWidth >= 1280) {
+  if (
+    !event.target.closest('#slideNavigation') &&
+    window.innerWidth >= DESKTOP_RESOLUTION
+  ) {
     DROPDOWNS.forEach((dropdown) => {
       dropdown.classList.remove('active')
       dropdown.querySelector('.dropdown-content').style.maxHeight = null
@@ -66,16 +115,27 @@ function close_dropdown(event) {
   }
 }
 
-// This function prevents `transition` styles of elements when resizing the window
+// transition functions prevents `transition` styles of elements when resizing the window
 function transition_dropdown() {
-  DROPDOWN_CONTENTS.forEach((dropdownContent) => {
-    dropdownContent.classList.add('noTransition')
-    setTimeout(() => dropdownContent.classList.remove('noTransition'), 300)
-  })
-  DROPDOWN_BUTTONS.forEach((dropdownButton) => {
-    let dropdownArrow = dropdownButton.querySelector('.boxicons-chevron-down')
+  if (window.innerWidth >= DESKTOP_RESOLUTION) {
+    DROPDOWN_CONTENTS.forEach((dropdownContent) => {
+      dropdownContent.classList.add('no-transition')
+      remove_class(dropdownContent, 'no-transition', 300)
+    })
+    DROPDOWN_BUTTONS.forEach((dropdownButton) => {
+      let dropdownArrow = dropdownButton.querySelector('.boxicons-chevron-down')
 
-    dropdownArrow.classList.add('noTransition')
-    setTimeout(() => dropdownArrow.classList.remove('noTransition'), 300)
-  })
+      dropdownArrow.classList.add('no-transition')
+      remove_class(dropdownArrow, 'no-transition', 300)
+    })
+  }
+}
+
+function transition_slideNavigation() {
+  let slideNavigation = HEADER.querySelector('#slideNavigation')
+
+  if (window.innerWidth < DESKTOP_RESOLUTION) {
+    slideNavigation.classList.add('no-transition')
+    remove_class(slideNavigation, 'no-transition')
+  }
 }
